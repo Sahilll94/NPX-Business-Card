5bc51e8cf0c4d0fae21947bbe3cc693612aab4cb
Sahil <sahilk64555@gmail.com>
2025-02-07 10:29:58 +0530

    added

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("Sahil | Developer | Open Source Enthusiast | Competitive Programmer"));
+console.log(chalk.green("🚀 Let's connect and explore coding together!"));
+
+console.log("");
+
+console.log(chalk.cyan("Portfolio: ") + "https://sahilportfolio.me/");
+console.log(chalk.cyan("GitHub: ") + "https://github.com/Sahilll94");
+console.log(chalk.cyan("LinkedIn: ") + "https://www.linkedin.com/in/sahilll94");
+console.log(chalk.cyan("Twitter: ") + "https://x.com/Sa_hilll94");
+console.log(chalk.cyan("Hashnode: ") + "https://hashnode.com/@Sahilll94");
+console.log(chalk.cyan("Medium: ") + "https://medium.com/@sa_hilll94");
+
+console.log("");
+
+console.log(chalk.green("🌟 Keep Coding with Consistency! 🚀"));
